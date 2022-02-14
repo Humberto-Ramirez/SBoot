cade367eb09b42bb6bff156ae397e917b54cabff
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-02-13 18:48:47 -0600

    Enable feign clients

--- app/src/main/kotlin/com/crowd/it/App.kt
+++ app/src/main/kotlin/com/crowd/it/App.kt
@@ -2,10 +2,12 @@ package com.crowd.it
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class App
 
-fun main(args:Array<String>) {
-    runApplication<App>(*args)
+fun main(args: Array<String>) {
+	runApplication<App>(*args)
 }
