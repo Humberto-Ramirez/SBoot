3941e33752d62bcd12d6631e32633c4f305761bb
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-02-13 20:13:50 -0600

    Rename controller

---
+++ app/src/main/kotlin/com/crowd/it/controllers/AviationController.kt
@@ -0,0 +1,17 @@
+package com.crowd.it.controllers
+
+import com.crowd.it.clients.AviationClient
+import com.crowd.it.dtos.CountryDto
+import com.crowd.it.dtos.response.PaginatedDto
+import com.crowd.it.services.AviationService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CountryController(val aviationClient: AviationClient):AviationService {
+
+	override fun getCountries(limit: Int, offset: Int): PaginatedDto<CountryDto> {
+		return aviationClient.getCountries(limit,offset)
+	}
+
+}
