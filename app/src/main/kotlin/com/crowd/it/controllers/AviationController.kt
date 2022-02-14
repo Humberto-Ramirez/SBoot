1757a80e0f363d594eaad76fd319bdbc51762b43
Humberto Ramírez <humberto.rjdh@gmail.com>
2022-02-13 21:42:16 -0600

    Implement missing aviation endpoints

--- app/src/main/kotlin/com/crowd/it/controllers/AviationController.kt
+++ app/src/main/kotlin/com/crowd/it/controllers/AviationController.kt
@@ -1,8 +1,7 @@
 package com.crowd.it.controllers
 
 import com.crowd.it.clients.AviationClient
-import com.crowd.it.dtos.CityDto
-import com.crowd.it.dtos.CountryDto
+import com.crowd.it.dtos.*
 import com.crowd.it.dtos.response.PaginatedDto
 import com.crowd.it.services.AviationService
 import org.springframework.web.bind.annotation.RestController
@@ -18,4 +17,23 @@ class AviationController(val aviationClient: AviationClient) : AviationService {
 		return aviationClient.getCities(limit, offset)
 	}
 
+	override fun getAirlines(limit: Int, offset: Int): PaginatedDto<AirlineDto> {
+		return aviationClient.getAirlines(limit, offset)
+	}
+
+	override fun getAirports(limit: Int, offset: Int): PaginatedDto<AirportDto> {
+		return aviationClient.getAirports(limit, offset)
+	}
+
+	override fun getAirplanes(limit: Int, offset: Int): PaginatedDto<AirplaneDto> {
+		return aviationClient.getAirplanes(limit, offset)
+	}
+
+	override fun getAircraftTypes(limit: Int, offset: Int): PaginatedDto<AircraftTypeDto> {
+		return aviationClient.getAircraftTypes(limit, offset)
+	}
+
+	override fun getTaxes(limit: Int, offset: Int): PaginatedDto<AviationTaxDto> {
+		return aviationClient.getTaxes(limit, offset)
+	}
 }
--- app/src/main/kotlin/com/crowd/it/services/AviationService.kt
+++ app/src/main/kotlin/com/crowd/it/services/AviationService.kt
@@ -1,7 +1,6 @@
 package com.crowd.it.services
 
-import com.crowd.it.dtos.CityDto
-import com.crowd.it.dtos.CountryDto
+import com.crowd.it.dtos.*
 import com.crowd.it.dtos.response.PaginatedDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -14,10 +13,42 @@ interface AviationService {
 		@RequestParam(value = "offset") offset: Int
 	): PaginatedDto<CountryDto>
 
-
 	@GetMapping("/cities")
 	fun getCities(
 		@RequestParam(value = "limit") limit: Int,
 		@RequestParam(value = "offset") offset: Int
 	): PaginatedDto<CityDto>
+
+	@GetMapping("/airlines")
+	fun getAirlines(
+		@RequestParam(value = "limit") limit: Int,
+		@RequestParam(value = "offset") offset: Int
+	): PaginatedDto<AirlineDto>
+
+	@GetMapping("/airports")
+	fun getAirports(
+		@RequestParam(value = "limit") limit: Int,
+		@RequestParam(value = "offset") offset: Int
+	): PaginatedDto<AirportDto>
+
+	@GetMapping("/airplanes")
+	fun getAirplanes(
+		@RequestParam(value = "limit") limit: Int,
+		@RequestParam(value = "offset") offset: Int
+	): PaginatedDto<AirplaneDto>
+
+	@GetMapping("/aircraft_types")
+	fun getAircraftTypes(
+		@RequestParam(value = "limit") limit: Int,
+		@RequestParam(value = "offset") offset: Int
+	): PaginatedDto<AircraftTypeDto>
+
+
+	@GetMapping("/taxes")
+	fun getTaxes(
+		@RequestParam(value = "limit") limit: Int,
+		@RequestParam(value = "offset") offset: Int
+	): PaginatedDto<AviationTaxDto>
+
+
 }
